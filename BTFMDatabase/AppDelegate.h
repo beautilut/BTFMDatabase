348d5c57806786c387139180bc1341594e9175ab
beautilut <beautilut@gmail.com>
2017-06-06 11:37:07 +0800

    创建基本版本

---
+++ BTFMDatabase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BTFMDatabase
+//
+//  Created by Beautilut on 2017/6/6.
+//  Copyright © 2017年 beautilut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BTFMDatabase/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BTFMDatabase
+//
+//  Created by Beautilut on 2017/6/6.
+//  Copyright © 2017年 beautilut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
